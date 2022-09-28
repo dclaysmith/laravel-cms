7c04718beec92e673641de932f4afe87bff0ea83
D Clay Smith <dclaysmith@gmail.com>
2022-09-28 20:25:54 +0000

    Refactoring

--- Project/packages/LaravelCms/deploy.sh
+++ Project/packages/LaravelCms/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Run Build"
 npm run build
-echo "cd ../Project"
-cd ../Project
+echo "cd ../../"
+cd ../../
 echo "Update Project"
 # composer update; 
 rm -rf public/vendor/laravel-cms; php artisan vendor:publish --provider="Dclaysmith\LaravelCms\Providers\LaravelCmsServiceProvider"
 echo "cd ../Package"
-cd ../Package
+cd ./packages/LaravelCms/
